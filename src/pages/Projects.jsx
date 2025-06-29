ef9b5c722fb0692738edb2033c969a6277ab7469
claracayres <0123m.m4567@gmail.com>
2025-06-29 16:18:59 -0400

    up test

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
                         <div className="flex justify-center">
                             <div className="project-card card rounded-xl overflow-hidden max-w-md">
                                 <div className="h-48 bg-gradient-to-br from-pink to-purple flex items-center justify-center">
-                                        <img src="./src/assets/fisioella-project.png" alt="Fisioella Web Site Project" />
+                                        <img src="/src/assets/fisioella-project.png" alt="Fisioella Web Site Project" />
                                     {/* <svg className="w-20 h-20 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                                     </svg> */}
